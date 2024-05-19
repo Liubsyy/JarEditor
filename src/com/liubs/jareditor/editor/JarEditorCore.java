c3974e836394e53b6975ba5411009a1707637978
Liubsyy <liubsyy@gmail.com>
2024-05-19 10:24:28 +0800

    文案

--- src/com/liubs/jareditor/editor/JarEditorCore.java
+++ src/com/liubs/jareditor/editor/JarEditorCore.java
@@ -109,11 +109,11 @@ public class JarEditorCore {
         //编译器
         IMyCompiler myCompiler;
         if(StringUtils.isEmpty(javaHome)) {
-            //默认使用IDEA运行时自带的JDK编译，相对javac命令编译更快
-            //IDEA2020.3自带JDK11, IDEA2022.3自带JDK17
+            //默认使用运行时动态编译，基于IDEA运行时自带的JDK编译，相对javac命令编译更快
+            //比如IDEA2020.3自带JDK11, IDEA2022.3自带JDK17
             myCompiler = new MyRuntimeCompiler(JavacToolProvider.getJavaCompilerFromProjectSdk());
         }else {
-            //为什么要用javac编译而不是直接用MyRuntimeCompiler动态编译呢？
+            //javac外部命令编译，为什么还用javac编译而是全部用上面的运行时动态编译呢？
             //首先有一个前提：插件运行在IDEA自带JDK上, 比如: IDEA2020.3自带JDK11, IDEA2022.3自带JDK17
             //假如IDEA2020.3去编译JDK17的话是有问题的，因为IDEA2020.3自带JDK11,而JDK11是无法加载JDK17的类库进行动态编译的
             //这张方案看似很low，但却是比较靠谱比较稳定的方案
