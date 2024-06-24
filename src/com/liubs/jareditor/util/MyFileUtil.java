d9268bbe86805078457607772f5723537c53d80f
Liubsyy <liubsyy@gmail.com>
2024-06-24 22:10:44 +0800

    v1.5.1

--- src/com/liubs/jareditor/util/MyFileUtil.java
+++ src/com/liubs/jareditor/util/MyFileUtil.java
@@ -3,6 +3,10 @@ package com.liubs.jareditor.util;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author Liubsyy
@@ -33,4 +37,26 @@ public class MyFileUtil {
             e.printStackTrace();
         }
     }
+
+
+    public static List<String> findAllJars(String path) {
+        List<String> jars = new ArrayList<>();
+        if(null ==path) {
+            return jars;
+        }
+
+        // 使用try-with-resources确保流会被正确关闭
+        try (Stream<Path> files = Files.walk(Paths.get(path))) {
+            jars = files
+                    .filter(p -> p.toString().endsWith(".jar"))  // 过滤出所有.jar文件
+                    .map(p -> p.toAbsolutePath().toString())  // 转换为绝对路径的字符串
+                    .collect(Collectors.toList());  // 将结果收集到列表中
+
+        } catch (IOException e) {
+            e.printStackTrace();  // 打印异常信息
+        }
+
+        return jars;
+    }
+
 }
